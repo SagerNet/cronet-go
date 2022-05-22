2f008151c2fbd1d03afa9c8bad072d328487b4be
世界 <i@sekai.icu>
2022-05-22 22:03:16 +0800

    Try build: add openwrt arm

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -57,22 +57,66 @@ func main() {
 		appendEnv("CGO_LDFLAGS", "-I $PWD --sysroot=$PWD/sysroot")
 		appendEnv("CGO_LDFLAGS", "-fuse-ld=lld")
 		if openwrt != "" {
+			appendEnv("CGO_CFLAGS", "-Wno-error=unused-command-line-argument")
+
+			if strings.HasPrefix(openwrt, "aarch64") {
+				appendEnv("CGO_CFLAGS", "--target=aarch64-openwrt-linux-musl -mbranch-protection=pac-ret")
+				appendEnv("CGO_LDFLAGS", "--target=aarch64-openwrt-linux-musl")
+			} else if strings.HasPrefix(openwrt, "arm") {
+				appendEnv("CGO_CFLAGS", "--target=arm-openwrt-linux-muslgnueabi")
+				appendEnv("CGO_LDFLAGS", "--target=arm-openwrt-linux-muslgnueabi")
+			}
+
+			var armIsSoft bool
+
 			switch openwrt {
-			case "x86":
-				appendEnv("CGO_CFLAGS", "--target=i486-openwrt-linux-musl -m32 -mfpmath=sse -msse3")
-				appendEnv("CGO_LDFLAGS", "-Wl,--dynamic-linker=/lib/ld-musl-i386.so.1 --target=i486-openwrt-linux-musl -m32")
 			case "x86_64":
 				appendEnv("CGO_CFLAGS", "--target=x86_64-openwrt-linux-musl -m64 -march=x86-64 -msse3")
 				appendEnv("CGO_LDFLAGS", "--target=x86_64-openwrt-linux-musl -m64")
-			case "aarch64_generic":
-				appendEnv("CGO_CFLAGS", "--target=aarch64-openwrt-linux-musl -mbranch-protection=pac-ret")
-				appendEnv("CGO_LDFLAGS", "--target=aarch64-openwrt-linux-musl")
+			case "x86":
+				appendEnv("CGO_CFLAGS", "--target=i486-openwrt-linux-musl -m32 -mfpmath=sse -msse3")
+				appendEnv("CGO_LDFLAGS", "-Wl,--dynamic-linker=/lib/ld-musl-i386.so.1 --target=i486-openwrt-linux-musl -m32")
+
 			case "aarch64_cortex-a53":
-				appendEnv("CGO_CFLAGS", "--target=aarch64-openwrt-linux-musl -mcpu=cortex-a53 -mbranch-protection=pac-ret")
-				appendEnv("CGO_LDFLAGS", "--target=aarch64-openwrt-linux-musl")
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a53")
 			case "aarch64_cortex-a72":
-				appendEnv("CGO_CFLAGS", "--target=aarch64-openwrt-linux-musl -mcpu=cortex-a72 -mbranch-protection=pac-ret")
-				appendEnv("CGO_LDFLAGS", "--target=aarch64-openwrt-linux-musl")
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a72")
+			case "aarch64_generic":
+				break
+
+			case "arm_cortex-a5_vfpv4":
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a5 -mfpu=vfpv4")
+			case "arm_cortex-a7":
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a7")
+				armIsSoft = true
+			case "arm_cortex-a7_neon-vfpv4":
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a7 -mfpu=neon-vfpv4")
+			case "arm_cortex-a8_vfpv3":
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a8 -mfpu=neon-vfpv3")
+			case "arm_cortex-a9":
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a9")
+				armIsSoft = true
+			case "arm_cortex-a9_neon":
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a9 -mfpu=neon")
+			case "arm_cortex-a9_vfpv3-d16":
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a9 -mfpu=vfpv3-d16")
+			case "arm_cortex-a15_neon-vfpv4":
+				appendEnv("CGO_CFLAGS", "-mcpu=cortex-a15 -mfpu=neon-vfpv4")
+			case "arm_arm1176jzf-s_vfp":
+				appendEnv("CGO_CFLAGS", "-mcpu=arm1176jzf-s -mfpu=vfp")
+			case "arm_arm926ej-s":
+				appendEnv("CGO_CFLAGS", "-mcpu=arm926ej-s -marm")
+				armIsSoft = true
+			}
+
+			if strings.HasPrefix(openwrt, "arm") {
+				if armIsSoft {
+					appendEnv("CGO_CFLAGS", "-mfloat-abi=soft")
+					appendEnv("CGO_LDFLAGS", "-mfloat-abi=soft")
+				} else {
+					appendEnv("CGO_CFLAGS", "-mfloat-abi=hard")
+					appendEnv("CGO_LDFLAGS", "-mfloat-abi=hard")
+				}
 			}
 		} else {
 			switch goarch {
@@ -86,8 +130,8 @@ func main() {
 				appendEnv("CGO_CFLAGS", "--target=aarch64-linux-gnu -mbranch-protection=pac-ret")
 				appendEnv("CGO_LDFLAGS", "--target=aarch64-linux-gnu")
 			case "arm":
-				appendEnv("CGO_CFLAGS", "--target=arm-linux-gnueabihf -march=armv7-a -mfloat-abi=hard -mtune=generic-armv7-a -mfpu=neon")
-				appendEnv("CGO_LDFLAGS", "--target=arm-linux-gnueabihf -march=armv7-a -mfloat-abi=hard")
+				appendEnv("CGO_CFLAGS", "--target=arm-linux-gnueabihf -march=armv7-a -mtune=generic-armv7-a -mfpu=neon")
+				appendEnv("CGO_LDFLAGS", "--target=arm-linux-gnueabihf -march=armv7-a ")
 				os.Setenv("GOARM", "7")
 			case "mipsle":
 				appendEnv("CGO_CFLAGS", "--target=mipsel-linux-gnu -march=mipsel -mcpu=mips32 -mhard-float")
@@ -103,6 +147,10 @@ func main() {
 	os.Setenv("CC", "clang")
 	os.Setenv("CGO_ENABLED", "1")
 	os.Setenv("CGO_LDFLAGS_ALLOW", ".*")
+
+	for _, env := range os.Environ() {
+		println(env)
+	}
 
 	args = append(args, os.Args[1:]...)
 
